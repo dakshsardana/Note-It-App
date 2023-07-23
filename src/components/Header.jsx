16ae85892949a3a64c3fbd81f711f70dd0e84430
Daksh Sardana <daksh2003sardana@gmail.com>
2023-07-23 14:15:19 +0530

    Updated

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() {
+   return <div><header>
+        <h1>Notesify </h1>
+   </header>
+    </div>
+}
+
+export default Header;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Note(){
+    return<div className='note'>
+        <h1>This is the heading</h1>
+        <p>This is the note</p>
+    </div>
+}
+
+export default Note;
